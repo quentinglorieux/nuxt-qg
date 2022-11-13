5cc5bd2b520862d101865ac68f11ef3f18b1579b
Quentin Glorieux <quentin.glorieux@gmail.com>
2022-11-13 21:14:40 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,53 @@
+export default defineNuxtConfig({
+  // https://v3.nuxtjs.org/guide/directory-structure/nuxt.config/
+
+  // As of RC12 Nuxt 3 supports Hybrid rendering mode
+  // https://v3.nuxtjs.org/guide/concepts/rendering#route-rules
+  //   routeRules: {
+  //     '/pages/**': { swr: true },
+  //     '/posts/**': { static: true },
+  //   },
+
+  css: [],
+
+  buildModules: [
+    // https://pinia.esm.dev
+    '@pinia/nuxt',
+    // https://vueuse.org/
+    '@vueuse/nuxt',
+  ],
+
+  modules: ['@nuxtjs/tailwindcss'],
+
+  runtimeConfig: {
+    public: {
+      directusUrl: process.env.DIRECTUS_URL,
+    },
+  },
+
+  postcss: {
+    plugins: {
+      'postcss-import': {},
+      'tailwindcss/nesting': {},
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  build: {
+    transpile: ['@heroicons/vue', '@headlessui/vue'],
+  },
+
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@heroicons/vue/20/solid',
+        '@heroicons/vue/24/solid',
+        '@heroicons/vue/24/outline',
+        '@headlessui/vue',
+        'vue',
+        'pinia',
+      ],
+    },
+  },
+})
